51352519f36213e2468d985ec4c901170febec37
Jakob Hoeg Mørk <jakmoe01@easv365.dk>
2024-02-11 20:18:30 +0100

    Sidebar - delete chat

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import { MoreHorizontal, SquarePen } from "lucide-react";
+import { MoreHorizontal, SquarePen, Trash2 } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { Button, buttonVariants } from "@/components/ui/button";
 import { Message } from "ai/react";
@@ -13,6 +13,20 @@ import UserSettings from "./user-settings";
 import { useLocalStorageData } from "@/app/hooks/useLocalStorageData";
 import { ScrollArea, Scrollbar } from "@radix-ui/react-scroll-area";
 import PullModel from "./pull-model";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { TrashIcon } from "@radix-ui/react-icons";
 
 interface SidebarProps {
   isCollapsed: boolean;
@@ -139,15 +153,48 @@ export function Sidebar({
                       </span>
                     </div>
                   </div>
-                  <MoreHorizontal
-                    size={15}
-                    className="ml-4 shrink-0 flex"
-                    onClick={(e) => {
-                      e.preventDefault();
-                      e.stopPropagation();
-                      handleDeleteChat(chatId);
-                    }}
-                  />
+                  <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                      <Button
+                        variant="ghost"
+                        className="flex justify-end items-center"
+                      >
+                        <MoreHorizontal size={15} className="shrink-0" />
+                      </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent className=" ">
+                      <Dialog>
+                        <DialogTrigger asChild>
+                          <Button
+                            variant="ghost"
+                            className="w-full flex gap-2 hover:text-red-500 text-red-500 justify-start items-center"
+                          >
+                            <Trash2 className="shrink-0 w-4 h-4" />
+                            Delete chat
+                          </Button>
+                        </DialogTrigger>
+                        <DialogContent>
+                          <DialogHeader className="space-y-4">
+                            <DialogTitle>Delete chat?</DialogTitle>
+                            <DialogDescription>
+                              Are you sure you want to delete this chat? This action cannot be undone.
+                            </DialogDescription>
+                            <div className="flex justify-end gap-2">
+                            <Button variant="outline">
+                                Cancel
+                              </Button>
+                              <Button
+                                variant="destructive"
+                                onClick={() => handleDeleteChat(chatId)}
+                              >
+                                Delete
+                              </Button>
+                            </div>
+                          </DialogHeader>
+                        </DialogContent>
+                      </Dialog>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
                 </Link>
               ))}
             </div>
