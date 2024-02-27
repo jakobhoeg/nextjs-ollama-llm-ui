3b00e0fecff9f71443f908ac4314af707207da42
chiu <chiu0602@gmail.com>
2024-02-27 10:53:23 +0800

    removed unused import

--- src/app/api/tags/route.ts
+++ src/app/api/tags/route.ts
@@ -1,9 +1,3 @@
-import { StreamingTextResponse, Message } from "ai";
-import { ChatOllama } from "@langchain/community/chat_models/ollama";
-import { AIMessage, HumanMessage } from "@langchain/core/messages";
-import { BytesOutputParser } from "@langchain/core/output_parsers";
-
-
 export async function GET(req: Request) {
   const res = await fetch(
     process.env.OLLAMA_URL + "/api/tags"
