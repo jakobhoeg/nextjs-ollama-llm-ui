6373db8c9cb49955fbea0f43fcee25eced2e9b97
Jakob Hoeg <jakmoe01@easv365.dk>
2024-09-21 12:24:57 +0200

    chore: update packages + simplify chat route

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,31 +1,25 @@
-import { StreamingTextResponse, Message } from "ai";
-import { ChatOllama } from "@langchain/community/chat_models/ollama";
-import { AIMessage, HumanMessage } from "@langchain/core/messages";
-import { BytesOutputParser } from "@langchain/core/output_parsers";
+import { createOllama } from 'ollama-ai-provider';
+import { streamText, convertToCoreMessages, CoreMessage } from 'ai'
 
 export const runtime = "edge";
 export const dynamic = "force-dynamic";
 
+
 export async function POST(req: Request) {
   const { messages, selectedModel } = await req.json();
 
-  const model = new ChatOllama({
-    baseUrl: process.env.NEXT_PUBLIC_OLLAMA_URL || "http://localhost:11434",
-    model: selectedModel,
+  const coreMessages = convertToCoreMessages(messages);
+
+  const ollama = createOllama({
   });
 
-  const parser = new BytesOutputParser();
+  const result = await streamText({
+    model: ollama(selectedModel),
+    messages: [
+      ...coreMessages
+    ],
+    system: 'You are a helpful chatbot.',
+  });
 
-  const stream = await model
-    .pipe(parser)
-    .stream(
-      (messages as Message[]).map((m) =>
-        m.role == "user"
-          ? new HumanMessage(m.content)
-          : new AIMessage(m.content)
-      )
-    );
-
-
-  return new StreamingTextResponse(stream);
+  return result.toDataStreamResponse()
 }
