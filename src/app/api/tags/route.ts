a90834232972095d1c1e1db420454071e2a69222
Jakob Hoeg <jakmoe01@easv365.dk>
2025-01-29 12:35:08 +0100

    remove NEXT_PUBLIC prefix from earlier version

--- src/app/api/tags/route.ts
+++ src/app/api/tags/route.ts
@@ -2,7 +2,8 @@ export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
 export async function GET(req: Request) {
-  const OLLAMA_URL = process.env.NEXT_PUBLIC_OLLAMA_URL || "http://127.0.0.1:11434";
+  const OLLAMA_URL = process.env.OLLAMA_URL;
+  console.log('OLLAMA_URL:', process.env.OLLAMA_URL);
   const res = await fetch(
     OLLAMA_URL + "/api/tags"
   );
